ddf06a4d05a7b145c540d0ffec20c66938f0f601
Millyn <9047230@qq.com>
2020-03-24 12:48:42 +0800

    timestamp convert

--- db/db.go
+++ db/db.go
@@ -44,7 +44,7 @@ func GetDb() *gorm.DB {
 		return nil
 	}
 
-	args := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8", dataSource.Username, dataSource.Password, dataSource.Host,dataSource.Port, dataSource.Database)
+	args := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Asia%2fShanghai", dataSource.Username, dataSource.Password, dataSource.Host,dataSource.Port, dataSource.Database)
 	//args := "root:root@tcp(localhost:3306)/testoceango?charset=utf8"
 	db, err = gorm.Open(dataSource.DriverName, args)
 	if err != nil {
